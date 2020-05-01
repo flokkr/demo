a8523015e468a221951363a9d6229bc8559b2203
Elek Márton <elek@apache.org>
2020-05-01 11:24:53 +0200

    updated flekszible source file generation, build all

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-files=(hadoop/Flekszible ozone/Flekszible ozone-secure/Flekszible);
-for file in ${files[@]}; do
+for file in $(find -name Flekszible); do
     DIR=$(dirname $file)
     echo "Generating files from/to $DIR"
     cd $DIR
